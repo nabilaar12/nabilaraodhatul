bbb7eaf37b6e10c6ea2f0bd1de68001c470852fe
Nabilaraodhatul <raodhatulnabila@gmail.com>
2025-04-30 12:03:08 +0000

    bilaa

---
+++ penjumlahan angka.py
@@ -0,0 +1,9 @@
+# Input dari pengguna
+angka1 = int(input("Masukkan angka pertama: "))
+angka2 = int(input("Masukkan angka kedua: "))
+
+# Proses penjumlahan
+jumlah = angka1 + angka2
+
+# Output hasil
+print("Hasil penjumlahan:", jumlah)
